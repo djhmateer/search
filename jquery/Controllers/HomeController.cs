6f07d3caa98198326412fe71e588efde96419a44
Dave Mateer <davemateer@gmail.com>
2010-02-18 18:04:45 +1300

    first commit - not working yet live

---
+++ jquery/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace jquery.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Search(string searchTerm)
+        {
+            if (searchTerm == string.Empty)
+            {
+                return View();
+            }
+            else
+            {
+                // if the search contains only one result return details
+                // otherwise a list
+                var artists = from a in Db()
+                              where a.Contains(searchTerm)
+                              orderby a
+                              select a;
+
+                if (artists.Count() == 0)
+                {
+                    return View("notfound");
+                }
+
+                if (artists.Count() > 1)
+                {
+                    return View("List", artists);
+                }
+                else
+                {
+                    return RedirectToAction("Details", new {id = artists.First()});
+                }
+            }
+        }
+
+        public ActionResult Details(string id) {
+            ViewData["Message"] = "You selected: " + id;
+            return View();
+        }
+
+        public ActionResult getAjaxResult(string q)
+        {
+            string searchResult = string.Empty;
+
+            var results = (from a in Db()
+                           where a.Contains(q)
+                           orderby a
+                           select a).Take(10);
+
+            foreach (string a in results)
+            {
+                searchResult += string.Format("{0}|\r\n", a);
+            }
+
+            return Content(searchResult);
+        }
+
+        IList<string> Db()
+        {
+            List<string> results = new List<string>();
+            results.Add("apple");
+            results.Add("advocado");
+            results.Add("aardvark");
+            results.Add("banana");
+            results.Add("orange");
+            return results;
+        }
+    }
+}
---
+++ jquery/Views/Home/Index.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage" %>
+
+<asp:Content ID="indexTitle" ContentPlaceHolderID="TitleContent" runat="server">
+Home
+</asp:Content>
+
+<asp:Content ID="indexContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    
+  
+
+    <h2>Search for something</h2>
+    <form action="/Home/Search" method="post" id="searchForm">
+      <input type="text" name="searchTerm" id="searchTerm" value="" size="10" maxlength ="30" />
+      <input type ="submit" value="Search" />
+    </form>
+    <br />
+
+</asp:Content>
